ac2576ac813d560aba6db90b8b834b2b0625c628
Alan Johnson <nalanj@fastmail.com>
2025-05-07 07:57:32 -0400

    It seems not using systemPrompt gets functions calling again

--- index.ts
+++ index.ts
@@ -16,7 +16,15 @@ process.on("SIGINT", () => {
 
 const configDir = path.join(os.homedir(), ".config", "codebud");
 const modelDir = path.join(configDir, "models");
-const modelUri = "hf:mradermacher/DeepSeek-R1-Distill-Qwen-14B-GGUF:Q4_K_M";
+
+// medium model
+//const modelUri = "hf:mradermacher/DeepSeek-R1-Distill-Qwen-14B-GGUF:Q4_K_M";
+
+// small model
+const modelUri = "hf:mradermacher/DeepSeek-R1-Distill-Qwen-7B-GGUF:Q4_K_M";
+
+// large model
+// const modelUri = "hf:mradermacher/DeepSeek-R1-Distill-Qwen-32B-GGUF:Q4_K_M";
 
 console.log("Loading model...");
 const llama = await getLlama();
@@ -29,33 +37,38 @@ console.log("Creating context...");
 const context = await model.createContext();
 const session = new LlamaChatSession({
 	contextSequence: context.getSequence(),
-	systemPrompt: `
-		- You are a helpful programming assistant.
-		- You're able to call the available functions to help the user with their tasks. 
-		- You work with fairly advanced programmers, so you can be concise and use technical language.
-		- You must always base your answers on the information provided in the task
-		  and the state of the codebase as provided by the results of the given functions.
-		- The progammers you work with want you to bias toward actions. Call functions
-			when needed and answer their questions explicity.
-	`,
 });
 
 console.log("Submitting prompt...");
-const a1 = await session.promptWithMeta(process.argv[2], {
-	functions,
-	onResponseChunk(chunk) {
-		const isThoughtSegment =
-			chunk.type === "segment" && chunk.segmentType === "thought";
+const a1 = await session.promptWithMeta(
+	`
+		Background:
+			- You are a helpful programming assistant.
+			- You're able to call the available functions to help the user with their tasks. 
+			- You work with fairly advanced programmers, so you can be concise and use technical language.
+			- You must always base your answers on the information provided in the task
+				and the state of the codebase as provided by the results of the given functions.
+			- The progammers you work with want you to bias toward actions. Call functions
+				when needed and answer their questions explicity.
+		Task:
+			${process.argv[2]}
+	`,
+	{
+		functions,
+		onResponseChunk(chunk) {
+			const isThoughtSegment =
+				chunk.type === "segment" && chunk.segmentType === "thought";
 
-		if (chunk.type === "segment" && chunk.segmentStartTime != null)
-			process.stdout.write(` [segment start: ${chunk.segmentType}] `);
+			if (chunk.type === "segment" && chunk.segmentStartTime != null)
+				process.stdout.write(` [segment start: ${chunk.segmentType}] `);
 
-		process.stdout.write(chunk.text);
+			process.stdout.write(chunk.text);
 
-		if (chunk.type === "segment" && chunk.segmentEndTime != null)
-			process.stdout.write(` [segment end: ${chunk.segmentType}] `);
+			if (chunk.type === "segment" && chunk.segmentEndTime != null)
+				process.stdout.write(` [segment end: ${chunk.segmentType}] `);
+		},
 	},
-});
+);
 
 const fullResponse = a1.response
 	.map((item) => {
